8bc8418b83dce637ab398f2dab179ff3056ca310
Paulo Trevizan <paulotrevizan@gmail.com>
2026-01-12 15:05:03 +0000

    chore: bootstrap spring boot project

---
+++ src/main/java/com/trevizan/mithrilledger/MithrilledgerApplication.java
@@ -0,0 +1,13 @@
+package com.trevizan.mithrilledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MithrilledgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MithrilledgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trevizan/mithrilledger/MithrilledgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trevizan.mithrilledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MithrilledgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
