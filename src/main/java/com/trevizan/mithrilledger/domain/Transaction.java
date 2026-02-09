0bb4c75168cdd9169fbdcfd62612698c5d45a3d5
Paulo Trevizan <paulotrevizan@gmail.com>
2026-02-09 12:17:45 +0000

    feat: create Transaction entity

---
+++ src/main/java/com/trevizan/mithrilledger/domain/Transaction.java
@@ -0,0 +1,77 @@
+package com.trevizan.mithrilledger.domain;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "from_wallet_id")
+    private Wallet fromWallet;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "to_wallet_id")
+    private Wallet toWallet;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal amount;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    protected Transaction() {
+
+    }
+
+    public Transaction(Wallet fromWallet, Wallet toWallet, BigDecimal amount) {
+        if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Amount must be positive.");
+        }
+
+        if (fromWallet.equals(toWallet)) {
+            throw new IllegalArgumentException("Origin and Destination Wallet must be different.");
+        }
+
+        this.fromWallet = fromWallet;
+        this.toWallet = toWallet;
+        this.amount = amount;
+        this.createdAt = Instant.now();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Wallet getFromWallet() {
+        return fromWallet;
+    }
+
+    public Wallet getToWallet() {
+        return toWallet;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+}
